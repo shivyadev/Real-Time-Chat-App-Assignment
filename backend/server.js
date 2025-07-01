9b19e89a5349cfa3bb63633d229b4c92e6dfdf79
Shivansh <shivanshyadav12@gmail.com>
2025-07-01 22:52:46 +0530

    Removed even more console logs

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,7 @@ const wss = new WebSocket.Server({ server });
 dotenv.config();
 
 wss.on("connection", async (ws) => {
-  console.log("Client connected");
-  console.log("Total clients:", wss.clients.size);
+  console.log("Client connected, Total clients: ", wss.clients.size);
 
   // Fetching the previous chat history and sending them back to client
   const history = await ChatHistory.find()
@@ -34,7 +33,6 @@ wss.on("connection", async (ws) => {
   ws.on("message", async (data) => {
     try {
       const { username, message } = JSON.parse(data);
-      console.log("Received message:", message);
 
       // Receiving the message sent from the client/frontend and broadcasting to all active client
       const messageToSend = {
@@ -68,8 +66,7 @@ wss.on("connection", async (ws) => {
   });
 
   ws.on("close", () => {
-    console.log("Client disconnected");
-    console.log("Remaining clients:", wss.clients.size);
+    console.log("Client disconnected, Remaining clients:", wss.clients.size);
   });
 
   ws.on("error", (err) => {
